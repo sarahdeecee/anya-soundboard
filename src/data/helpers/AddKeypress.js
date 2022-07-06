efff7ebbf77db11056ca3ecf50f67b1cfc8d5ba9
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-07-05 20:09:03 -0400

    add function to determine keypresses for a number of samples

---
+++ src/data/helpers/AddKeypress.js
@@ -0,0 +1,31 @@
+const keypresses = {
+  row1: ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
+  row2: ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';'],
+  row3: ['z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/']
+};
+
+// Choose keys closest to each other
+const addKeypress = selectedSamples => {
+  // Calculate length for each keyboard row
+  const rowLength = Math.ceil(selectedSamples.length / 3);
+  
+  // Add keypress for each sample
+  samplesWithKeypress = selectedSamples.map((sample, index) => {
+    // First keyboard row
+    if (index < rowLength) {
+      return {...sample, keypress: keypresses.row1[index]}
+    }
+    // Second keyboard row
+    if (index >= rowLength && index < rowLength * 2) {
+      return {...sample, keypress: keypresses.row2[index - rowLength]}
+    }
+    // Third keyboard row
+    if (index >= rowLength * 2 && index < rowLength * 3) {
+      return {...sample, keypress: keypresses.row3[index - rowLength * 2]}
+    }
+  })
+
+  return samplesWithKeypress;
+};
+
+// export default addKeypress;
