9eb9ecc74b7c0e7fa32279897815b182a65a8650
Sarah Dela Cruz <sarah.delacruz@gmail.com>
2022-08-08 16:18:37 -0400

    optimize images for smaller filesize

--- src/data/Samples.js
+++ src/data/Samples.js
@@ -4,168 +4,168 @@ const sampleData = [
     japanese: 'ワクワク...!',
     episode: '1',
     file: '../../01-wakuwaku.mp3',
-    picture: '../../01-wakuwaku.png'
+    picture: '../../01-wakuwaku.jpg'
   },
   { english: 'K!',
     romaji: 'ui',
     japanese: 'うい',
     episode: '1',
     file: '../../01-ui.mp3',
-    picture: '../../01-ui.png'
+    picture: '../../01-ui.jpg'
   },
   { english: 'Bomby bomb, bomby bomb!',
     romaji: 'bakudan bakudan!',
     japanese: '爆弾、爆弾！',
     episode: '1',
     file: '../../01-bakudan.mp3',
-    picture: '../../01-bakudan.png'
+    picture: '../../01-bakudan.jpg'
   },
   { english: 'Peanuts!',
     romaji: 'Peanuts!',
     japanese: 'ピーナッツ！',
     episode: '1',
     file: '../../01-peanuts.mp3',
-    picture: '../../01-peanuts.png'
+    picture: '../../01-peanuts.jpg'
   },
   { english: 'I don\'t wanna!!',
     romaji: 'iyaaaadaaaa!!',
     japanese: 'いや〜だぁー！！',
     episode: '1',
     file: '../../01-yada.mp3',
-    picture: '../../01-yada.png'
+    picture: '../../01-yada.jpg'
   },
   { english: 'Wah!',
     romaji: 'wah!',
     japanese: 'わっ！',
     episode: '1',
     file: '../../01-wah.mp3',
-    picture: '../../01-wah.png'
+    picture: '../../01-wah.jpg'
   },
   { english: 'I\'m getting sold off!',
     romaji: 'Anya, uritobasareru!',
     japanese: 'アーニャ、売り飛ばされる〜！',
     episode: '2',
     file: '../../02-uri.mp3',
-    picture: '../../02-uri.png'
+    picture: '../../02-uri.jpg'
   },
   { english: 'SO EXCITING!',
     romaji: 'WAKUWAKU',
     japanese: 'ワクワク！！',
     episode: '2',
     file: '../../02-wakuwaku.mp3',
-    picture: '../../02-wakuwaku.png'
+    picture: '../../02-wakuwaku.jpg'
   },
   { english: 'Ah~',
     romaji: 'Ah~',
     japanese: 'あ〜',
     episode: '2',
     file: '../../02-aa.mp3',
-    picture: '../../02-aa.png'
+    picture: '../../02-aa.jpg'
   },
   { english: 'Anya is lonely without a mother~',
     romaji: 'Anya, haha inakute samishii',
     japanese: 'アーニャ、母いなくて寂しい',
     episode: '2',
     file: '../../02-hahainakutesamishii.mp3',
-    picture: '../../02-hahainakutesamishii.png'
+    picture: '../../02-hahainakutesamishii.jpg'
   },
   { english: 'Anya longs for a mother~',
     romaji: 'Haha no sonzai koishii',
     japanese: '母の存在恋しい',
     episode: '2',
     file: '../../02-hahanosonzaikoishii.mp3',
-    picture: '../../02-hahanosonzaikoishii.png'
+    picture: '../../02-hahanosonzaikoishii.jpg'
   },
   { english: 'Stock on them peanuts!',
     romaji: 'peanuts kaikondoke!',
     japanese: 'ピーナッツ買い込んどけ！',
     episode: '2',
     file: '../../02-peanutskaikondoke.mp3',
-    picture: '../../02-peanutskaikondoke.png'
+    picture: '../../02-peanutskaikondoke.jpg'
   },
   { english: 'Gah!',
     romaji: 'GAN',
     japanese: 'ガーン！',
     episode: '3',
     file: '../../03-gan.mp3',
-    picture: '../../03-gan.png'
+    picture: '../../03-gan.jpg'
   },
   { english: 'An ooting, an ooting, la la la la',
     romaji: 'odekeke odekeke ran ra ra ran',
     japanese: 'お出けけお出けけらんらららん',
     episode: '3',
     file: '../../03-odekeke.mp3',
-    picture: '../../03-odekeke.png'
+    picture: '../../03-odekeke.jpg'
   },
   { english: 'She\'s all nakie!',
     romaji: 'supponpon supponpon!',
     japanese: 'スッポンポンスッポンポン！',
     episode: '3',
     file: '../../03-supponpon.mp3',
-    picture: '../../03-supponpon.png'
+    picture: '../../03-supponpon.jpg'
   },
   { english: 'Head chopped! Body chopped!',
     romaji: 'kubi chonpa karada chonpa',
     japanese: '首チョンパ体チョンパ',
     episode: '3',
     file: '../../03-kubichonpa.mp3',
-    picture: '../../03-kubichonpa.png'
+    picture: '../../03-kubichonpa.jpg'
   },
   { english: 'S\'all good, papa',
     romaji: 'donmai chichi',
     japanese: 'ドンマイ、父',
     episode: '3',
     file: '../../03-donmaichichi.mp3',
-    picture: '../../03-donmaichichi.png'
+    picture: '../../03-donmaichichi.jpg'
   },
   { english: 'They\'re like tiny bits of trash...',
     romaji: 'hito ga gomi no you da...',
     japanese: '人がゴミのようだ…',
     episode: '3',
     file: '../../03-hitogagomi.mp3',
-    picture: '../../03-hitogagomi.png'
+    picture: '../../03-hitogagomi.jpg'
   },
   { english: 'Baldy',
     romaji: 'hagechabe',
     japanese: 'ハゲちゃべ',
     episode: '4',
     file: '../../04-hagechabe.mp3',
-    picture: '../../04-hagechabe.png'
+    picture: '../../04-hagechabe.jpg'
   },
   { english: 'I really wanna pick my nose',
     romaji: 'anya, hanakuso hojiritai',
     japanese: 'アーニャ、鼻くそほじりたい',
     episode: '4',
     file: '../../04-hanakuso.mp3',
-    picture: '../../04-hanakuso.png'
+    picture: '../../04-hanakuso.jpg'
   },
   { english: 'It\'s okay, please',
     romaji: 'daijoubumasu',
     japanese: '大丈夫ます',
     episode: '4',
     file: '../../04-daijoubumasu.mp3',
-    picture: '../../04-daijoubumasu.png'
+    picture: '../../04-daijoubumasu.jpg'
   },
   { english: 'She\'s very nice.',
     romaji: 'tottemo yasashii desu',
     japanese: 'とっても優しいです',
     episode: '4',
     file: '../../04-yasashiidesu.mp3',
-    picture: '../../04-yasashiidesu.png'
+    picture: '../../04-yasashiidesu.jpg'
   },
   { english: 'But sometimes she\'s scary.',
     romaji: 'demo tama ni okkanai desu',
     japanese: 'でもたまにおっかいないです',
     episode: '4',
     file: '../../04-okkanaidesu.mp3',
-    picture: '../../04-okkanaidesu.png'
+    picture: '../../04-okkanaidesu.jpg'
   },
   { english: 'A perfect 100!',
     romaji: 'hyakuten manten desu!',
     japanese: '百点満点です！',
     episode: '4',
     file: '../../04-hyakutenmanten.mp3',
-    picture: '../../04-hyakutenmanten.png'
+    picture: '../../04-hyakutenmanten.jpg'
   },
   // 3:05
   { english: 'Because I stepped in poop...!',
@@ -173,7 +173,7 @@ const sampleData = [
     japanese: 'アーニャがウンコ踏んだから',
     episode: '5',
     file: '../../05-unko.mp3',
-    picture: '../../05-unko.png'
+    picture: '../../05-unko.jpg'
   },
   // 5:40
   { english: 'Anya will pass...!',
@@ -181,7 +181,7 @@ const sampleData = [
     japanese: 'アーニャ合格〜　アーニャ合格〜',
     episode: '5',
     file: '../../05-goukaku.mp3',
-    picture: '../../05-goukaku.png'
+    picture: '../../05-goukaku.jpg'
   },
   // 11:32
   { english: 'I\'m sad',
@@ -189,7 +189,7 @@ const sampleData = [
     japanese: 'アーニャしょんぼり',
     episode: '5',
     file: '../../05-shonbori.mp3',
-    picture: '../../05-shonbori.png'
+    picture: '../../05-shonbori.jpg'
   },
   // 13:24
   // { english: 'Can be whatever',
@@ -197,7 +197,7 @@ const sampleData = [
   //   japanese: '何でもいい',
   //   episode: '5',
   //   file: '../../05-nandemoii.mp3',
-  //   picture: '../../05-nandemoii.png'
+  //   picture: '../../05-nandemoii.jpg'
   // },
   // 16:08
   { english: 'I wanna see~',
@@ -205,14 +205,14 @@ const sampleData = [
     japanese: 'アーニャ、見たいなぁ〜',
     episode: '5',
     file: '../../05-mitaina.mp3',
-    picture: '../../05-mitaina.png'
+    picture: '../../05-mitaina.jpg'
   },
   { english: 'I wanna see Loidman~',
     romaji: 'kanpeki na Loidman, mitai na~',
     japanese: '完璧なロイドマン、見たいなぁ〜',
     episode: '5',
     file: '../../05-kanpekinaloidman.mp3',
-    picture: '../../05-kanpekinaloidman.png'
+    picture: '../../05-kanpekinaloidman.jpg'
   },
   // 19:21
   { english: 'Save me, Loidman~',
@@ -220,7 +220,7 @@ const sampleData = [
     japanese: '助けて、ロイドマ〜ン',
     episode: '5',
     file: '../../05-tasukete.mp3',
-    picture: '../../05-tasukete.png'
+    picture: '../../05-tasukete.jpg'
   },
   // 2:13
   { english: 'Fank you very please',
@@ -228,49 +228,49 @@ const sampleData = [
     japanese: 'あざざます！',
     episode: '6',
     file: '../../06-azazamasu.mp3',
-    picture: '../../06-azazamasu.png'
+    picture: '../../06-azazamasu.jpg'
   },
   { english: 'Haba nice day',
     romaji: 'iterasai',
     japanese: 'いてらさい',
     episode: '6',
     file: '../../06-iterasai.mp3',
-    picture: '../../06-iterasai.png'
+    picture: '../../06-iterasai.jpg'
   },
   { english: 'Am I cute?',
     romaji: 'anya kawaii?',
     japanese: 'アーニャかわいい？',
     episode: '6',
     file: '../../06-anyakawaii.mp3',
-    picture: '../../06-anyakawaii.png'
+    picture: '../../06-anyakawaii.jpg'
   },
   { english: 'Do dodo do dodo do dodo',
     romaji: 'run run run run run run',
     japanese: 'るんるん るんるん るんるん',
     episode: '6',
     file: '../../06-runrun.mp3',
-    picture: '../../06-runrun.png'
+    picture: '../../06-runrun.jpg'
   },
   // { english: 'There, there',
   //   romaji: 'yoshiyoshi',
   //   japanese: 'よしよし',
   //   episode: '6',
   //   file: '../../06-yoshiyoshi.mp3',
-  //   picture: '../../06-yoshiyoshi.png'
+  //   picture: '../../06-yoshiyoshi.jpg'
   // },
   // { english: 'Killer punch, kapow!',
   //   romaji: 'hissatsu panchi pokan!',
   //   japanese: '必殺パンチ　ポカーン！',
   //   episode: '6',
   //   file: '../../06-hissatsu.mp3',
-  //   picture: '../../06-hissatsu.png'
+  //   picture: '../../06-hissatsu.jpg'
   // },
   // { english: 'Hah!',
   //   romaji: 'sei!',
   //   japanese: 'せい！',
   //   episode: '6',
   //   file: '../../06-sei.mp3',
-  //   picture: '../../06-sei.png'
+  //   picture: '../../06-sei.jpg'
   // },
   // 16:15
   { english: 'Okie dokie!',
@@ -278,7 +278,7 @@ const sampleData = [
     japanese: 'オーキードーキー！',
     episode: '6',
     file: '../../06-okiedokie.mp3',
-    picture: '../../06-okiedokie.png'
+    picture: '../../06-okiedokie.jpg'
   },
   // 17:25 ninmu no tame Anya gaman
   // 18:18 heh
@@ -288,7 +288,7 @@ const sampleData = [
     japanese: 'アーニャがやった証拠あんのか？',
     episode: '6',
     file: '../../06-shouko.mp3',
-    picture: '../../06-shouko.png'
+    picture: '../../06-shouko.jpg'
   },
   // 8:54 kami-sama...?
   { english: 'Is that you, God...?',
@@ -296,7 +296,7 @@ const sampleData = [
     japanese: '神様…？',
     episode: '7',
     file: '../../07-kamisama.mp3',
-    picture: '../../07-kamisama.png'
+    picture: '../../07-kamisama.jpg'
   },
   // 9:04 nanka sugoi puressha kanjiru
   { english: 'I can feel an immense pressure...',
@@ -304,7 +304,7 @@ const sampleData = [
     japanese: 'なんかすごいプレッシャー感じる',
     episode: '7',
     file: '../../07-pressure.mp3',
-    picture: '../../07-pressure.png'
+    picture: '../../07-pressure.jpg'
   },
   // 12:19 gomennasai, anya tansoku de
   { english: 'I\'m sorry I have stubby legs...',
@@ -312,7 +312,7 @@ const sampleData = [
     japanese: 'ごめんなさい、アーニャ短足で',
     episode: '7',
     file: '../../07-tansoku.mp3',
-    picture: '../../07-tansoku.png'
+    picture: '../../07-tansoku.jpg'
   },
   // 13:50 gakkou kowai... bekkii...
   { english: 'School is scary... Becky...',
@@ -320,7 +320,7 @@ const sampleData = [
     japanese: '学校怖い…ベッキー…',
     episode: '7',
     file: '../../07-gakkoukowai.mp3',
-    picture: '../../07-gakkoukowai.png'
+    picture: '../../07-gakkoukowai.jpg'
   },
   // 15:38 benkyou iya
   { english: 'I hate studying!!',
@@ -328,7 +328,7 @@ const sampleData = [
     japanese: '勉強いやぁー！！',
     episode: '7',
     file: '../../07-benkyouiya.mp3',
-    picture: '../../07-benkyouiya.png'
+    picture: '../../07-benkyouiya.jpg'
   },
   // 21:22 haha... chichi wo koroshite wa dame...
   { english: 'No, mama... you can\'t kill papa...',
@@ -336,7 +336,7 @@ const sampleData = [
     japanese: '母…父を殺してはダメ…',
     episode: '7',
     file: '../../07-koroshitewadame.mp3',
-    picture: '../../07-koroshitewadame.png'
+    picture: '../../07-koroshitewadame.jpg'
   },
 ];
 
